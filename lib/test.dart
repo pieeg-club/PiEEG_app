5962b68a4f3aaaf8f09e123a16fafeafe7c19394
Romasav <roma.savitski@gmail.com>
2024-11-05 15:35:36 +0000

    test

--- lib/test.dart
+++ lib/test.dart
@@ -3,7 +3,6 @@ import 'dart:typed_data';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:rpi_gpio/gpio.dart';
 import 'package:rpi_gpio/rpi_gpio.dart';
 import 'package:rpi_spi/rpi_spi.dart';
 import 'package:rpi_spi/spi.dart';
@@ -83,7 +82,7 @@ class ADS1299Reader {
   Future<void> startDataRead() async {
     RpiGpio gpio = await initialize_RpiGpio(spi: false);
     const int buttonPin = 37;
-    final button = gpio.input(buttonPin, Pull.down);
+    final button = gpio.input(buttonPin);
     int testDRDY = 5;
 
     print('Rpigpio initialized');
