5a8e5615bdf14dc48569bcef7f05240a5daf83bd
Romasav <roma.savitski@gmail.com>
2024-11-05 11:01:02 +0000

    some logic changes

--- lib/test.dart
+++ lib/test.dart
@@ -80,7 +80,7 @@ class ADS1299Reader {
   }
 
   Future<void> startDataRead() async {
-    RpiGpio gpio = await initialize_RpiGpio();
+    RpiGpio gpio = await initialize_RpiGpio(spi: false);
     const int buttonPin = 26;
     final button = gpio.input(buttonPin);
     int testDRDY = 5;
