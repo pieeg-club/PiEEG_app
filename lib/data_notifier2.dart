dcc60bf33d159d6fc90568b9bed77cb643ce3086
Romasav <roma.savitski@gmail.com>
2024-11-08 16:14:41 +0000

    test

--- lib/data_notifier2.dart
+++ lib/data_notifier2.dart
@@ -7,15 +7,12 @@ class DataNotifier2 extends ChangeNotifier {
   var list = List<List<double>>.generate(8, (i) => []);
 
   void addData(List<List<double>> data) {
-    final voltData = <List<double>>[];
     for (var i = 0; i < 8; i++) {
-      voltData.add([]);
-      voltData[i] = [...list[i], ...data[i]];
-      while (voltData[i].length > 1000) {
-        voltData[i].removeAt(0);
+      list[i].addAll(data[i]);
+      while (list[i].length > 1000) {
+        list[i].removeAt(0);
       }
     }
-    list = voltData;
     notifyListeners();
   }
 }
