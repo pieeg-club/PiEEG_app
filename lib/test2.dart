f7dacded110607b417a450744c081cb7e1573340
Romasav <roma.savitski@gmail.com>
2024-11-08 15:12:44 +0000

    test

--- lib/test2.dart
+++ lib/test2.dart
@@ -217,8 +217,8 @@ class ADS1299Reader2 {
           // move data from buffer to dataToSend
           for (var i = 0; i < buffers.length; i++) {
             dataToSend[i] = buffers[i].getData();
-            final clearData = dataToSend[i].sublist(40, 80);
-            for (int j = 0; j < 40; j++) {
+            final clearData = dataToSend[i].sublist(0, 40);
+            for (int j = 40; j < 80; j++) {
               dataToSend[i][j] = clearData[j];
             }
           }
