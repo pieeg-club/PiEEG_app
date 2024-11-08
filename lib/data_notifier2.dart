a44aefaa8f7984d67184ffdabde6be4e1001b264
Romasav <roma.savitski@gmail.com>
2024-11-08 18:54:35 +0000

    test

--- lib/data_notifier2.dart
+++ lib/data_notifier2.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -14,6 +16,25 @@ class DataNotifier2 extends ChangeNotifier {
     for (var i = 0; i < 8; i++) {
       // Add new data to the end of each list[i]
       list[i].addAll(data[i]);
+
+      // Trim the list only if its length exceeds maxLength
+      if (list[i].length > maxLength) {
+        list[i] = list[i].sublist(list[i].length - maxLength);
+      }
+    }
+
+    randomData = !randomData;
+
+    notifyListeners();
+  }
+
+  Timer? _updateTimer;
+  final List<List<double>> _pendingData = List.generate(8, (_) => []);
+
+  void addSamples(List<double> samples) {
+    for (var i = 0; i < 8; i++) {
+      // Add new data to the end of each list[i]
+      list[i].add(samples[i]);
 
       // Trim the list only if its length exceeds maxLength
       if (list[i].length > maxLength) {
