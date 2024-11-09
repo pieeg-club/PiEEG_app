cb48dc6cc7855dc0603e89b718d3eaf4c72b494b
Romasav <roma.savitski@gmail.com>
2024-11-09 13:32:10 +0000

    test

--- lib/data_notifier2.dart
+++ lib/data_notifier2.dart
@@ -59,16 +59,14 @@ class DataNotifier2 extends ChangeNotifier {
   void updateData(List<double> data) {
     for (var i = 0; i < data.length; i++) {
       final controller = controllers[i]!;
-      // var removedDataIndex = -1;
       list[i].add(data[i]);
-      // if (list[i].length > maxLength) {
-      //   list[i].removeAt(0);
-      //   removedDataIndex = 0;
-      // }
-      controller.updateDataSource(
-        addedDataIndex: list[i].length - 1,
-        // removedDataIndex: removedDataIndex,
-      );
+      if (list[i].length >= maxLength) {
+        list[i].removeAt(0);
+        controller.updateDataSource(
+          addedDataIndex: list[i].length - 1,
+          removedDataIndex: 0,
+        );
+      }
     }
   }
 }
