49da511338b0de0c5d13cfa54683871d519cfc68
Romasav <roma.savitski@gmail.com>
2024-11-19 17:57:37 +0000

    test

--- lib/test2.dart
+++ lib/test2.dart
@@ -219,7 +219,7 @@ class ADS1299Reader2 {
     }
 
     final difference = (currentValue - _lastValidValue!).abs();
-    if (difference > 7825) {
+    if (difference > 3925) {
       return false;
     }
 
