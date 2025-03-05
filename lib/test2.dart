6f6b784496b074ed584886b3953f7a28f04c2c0b
Romasav <roma.savitski@gmail.com>
2025-03-05 12:41:40 +0000

    test

--- lib/test2.dart
+++ lib/test2.dart
@@ -208,6 +208,7 @@ class ADS1299Reader2 {
         // Read data from SPI
         final data = _readData(spi, 27);
 
+        print("Sending data to process");
         algorithm.processData(
           data,
           (String data) => fileStorage.checkAndSaveData(data: data),
